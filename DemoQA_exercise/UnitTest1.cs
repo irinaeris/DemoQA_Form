11eafe22699ed24862020cbab3db8568076b7ab0
irinaeris <72436931+irinaeris@users.noreply.github.com>
2021-01-06 19:55:51 +0200

    Add project files.

---
+++ DemoQA_exercise/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoQA_exercise
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
